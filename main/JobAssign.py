7590a88fdf96fff922366f5c787e7ce4d7cc50b2
Aaron Ramsey <aaronramsey2000@gmail.com>
2020-03-27 22:31:06 +0000

    Added further functionality

--- main/JobAssign.py
+++ main/JobAssign.py
@@ -13,6 +13,9 @@ TOKEN = os.getenv('DISCORD_TOKEN')
 bot = commands.Bot(command_prefix='!')
 client = discord.Client()
 
+def getFilename(person):
+    return "../files/" + person + ".txt"
+
 @bot.event
 async def on_ready():
     print(
@@ -21,31 +24,59 @@ async def on_ready():
 
 @bot.command(name="assignJob", help="assigns a job to a person")
 async def assignJob(ctx, person: str, job: str):
-    file = open("../files/" + person[3:21] + ".txt", "a")
+    file = open(getFilename(person[3:21]), "a")
     file.write(job + "\n")
     response = "Job assigned"
     file.close()
     await ctx.send(response)
 
 def getJobs(person):
-    file = open("../files/" + person + ".txt", "r")
+    file = open(getFilename(person), "r")
 
     response = "<@!" + person + "> these are the jobs you have to do \n"
     data = file.readlines()
     for line in data:
         response += line
+    file.close()
     return response
 
 @bot.command(name="seeJobs", help="gets the jobs for that user")
 async def seeJobs(ctx, person: str):
+    print(ctx)
     await ctx.send(getJobs(person[3:21]))
 
 @bot.command(name="seeMyJobs", help="gets you your jobs to do")
 async def seeMyJobs(ctx):
     await ctx.send(getJobs(str(ctx.author.id)))
 
+def jobDone(person, job):
+    file = open(getFilename(person), "r")
+    jobs = file.readlines()
+    file.close()
+
+    writeFile = open(getFilename(person), "w")
+    jobs2write = ""
+
+    for line in jobs:
+        if line != job + "\n":
+            jobs2write += line
+        else:
+            doneFile = open(getFilename(person + "-DONE"), "a")
+            doneFile.write(line)
+            doneFile.close()
+
+    writeFile.writelines(jobs2write)
+    writeFile.close()
+    return job + " removed from <@!" + person + ">'s to do list"
+
+@bot.command(name="doneMyJob", help="removes a job you have done")
+async def removeMyJob(ctx, job: str):
+    await ctx.send(jobDone(str(ctx.author.id), job))
+
 @bot.command(name="jobDone", help="removes a job when it is done")
-async def removeJob(ctx, person: str, job):
-    print(job)
+async def removeJob(ctx, person: str, job: str):
+    await ctx.send(jobDone(str(person[3:21]), job))
+
+
 
 bot.run(TOKEN)
